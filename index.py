e733211583065d0a9ce6ad3a99b1d41fbd155f65
DawidO <okondawid2000@gmail.com>
2024-01-14 17:48:21 +0100

    poprawki

--- index.py
+++ index.py
@@ -1,70 +1,99 @@
-from flask import Flask, request, send_file
+from flask import Flask, render_template, request, send_file
 from cryptography.fernet import Fernet
 import os
+import uuid
 
 app = Flask(__name__)
+key_location = 'key.key'
 
-# Ścieżka do folderu przechowującego przesłane pliki
-UPLOAD_FOLDER = '/AlgorytmyAPI/upload'
-# Ścieżka do folderu, z którego będziemy pobierać pliki
-DOWNLOAD_FOLDER = '/AlgorytmyAPI/download'
-# Ścieżka do pliku z kluczem szyfrującym
-KEY_FILE = '/AlgorytmyAPI/key.key'
 
-# Wygenerowanie klucza szyfrującego, jeśli nie istnieje
-def generate_key():
-    if not os.path.exists(KEY_FILE):
-        key = Fernet.generate_key()
-        with open(KEY_FILE, 'wb') as key_file:
-            key_file.write(key)
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# Wczytanie klucza szyfrującego
-def load_key():
-    return open(KEY_FILE, 'rb').read()
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
-    try:
-        generate_key()
-        key = load_key()
-        file = request.files['file']
-        
-        if file:
-            # Szyfrowanie przesłanego pliku
-            f = Fernet(key)
-            encrypted_data = f.encrypt(file.read())
-            
-            # Zapisanie zaszyfrowanego pliku
-            encrypted_filename = os.path.join(UPLOAD_FOLDER, file.filename + ".enc")
-            with open(encrypted_filename, 'wb') as encrypted_file:
-                encrypted_file.write(encrypted_data)
-            
-            return 'Plik został przesłany i zaszyfrowany.'
-        return 'Nie wybrano pliku.'
-    except Exception as e:
-        return 'Wystąpił błąd podczas przesyłania i zapisywania pliku: ' + str(e)
+def upload():
+    file = request.files['file']
+    filename = file.filename
 
-@app.route('/download/<filename>', methods=['GET'])
-def download_file(filename):
-    try:
+    key = load_key()
+    cipher = Fernet(key)
+
+    encrypted_filename = str(uuid.uuid4())
+    encrypted_data = cipher.encrypt(file.read())
+
+    write_encrypted_file(encrypted_filename, encrypted_data)
+
+    return {"message": "File uploaded and encrypted successfully", "file_id": encrypted_filename}
+
+
+@app.route('/download/<file_id>', methods=['GET'])
+def download(file_id):
+    key = load_key()
+    cipher = Fernet(key)
+
+    encrypted_data = read_encrypted_file(file_id)
+    decrypted_data = cipher.decrypt(encrypted_data)
+
+    save_decrypted_file(file_id, decrypted_data)
+
+    return send_file(f'tmp/{file_id}', as_attachment=True)
+
+
+@app.route('/delete/<file_id>', methods=['POST'])
+def delete(file_id):
+    delete_encrypted_file(file_id)
+    delete_decrypted_file(file_id)
+
+    return {"message": "File deleted successfully"}
+
+
+def load_key():
+    if not os.path.exists(key_location):
         generate_key()
-        key = load_key()
-        
-        # Odszyfrowanie pliku
-        encrypted_filename = os.path.join(UPLOAD_FOLDER, filename)
-        with open(encrypted_filename, 'rb') as encrypted_file:
-            encrypted_data = encrypted_file.read()
-            f = Fernet(key)
-            decrypted_data = f.decrypt(encrypted_data)
-        
-        # Zapisanie odszyfrowanego pliku
-        decrypted_filename = os.path.join(DOWNLOAD_FOLDER, filename[:-4])
-        with open(decrypted_filename, 'wb') as decrypted_file:
-            decrypted_file.write(decrypted_data)
-        
-        return send_file(decrypted_filename, as_attachment=True)
-    except Exception as e:
-        return 'Wystąpił błąd podczas pobierania i odszyfrowywania pliku: ' + str(e)
+    with open(key_location, 'rb') as key_file:
+        key = key_file.read()
+    return key
+
+
+def generate_key():
+    if not os.path.exists(key_location):
+        key = Fernet.generate_key()
+        with open(key_location, 'wb') as key_file:
+            key_file.write(key)
+
+
+def read_encrypted_file(file_id):
+    with open(f'encrypted_files/{file_id}', 'rb') as enc_file:
+        return enc_file.read()
+
+
+def write_encrypted_file(file_id, data):
+    if not os.path.exists('encrypted_files'):
+        os.makedirs('encrypted_files')
+    with open(f'encrypted_files/{file_id}', 'wb') as enc_file:
+        enc_file.write(data)
+
+
+def save_decrypted_file(file_id, data):
+    if not os.path.exists('tmp'):
+        os.makedirs('tmp')
+    with open(f'tmp/{file_id}', 'wb') as dec_file:
+        dec_file.write(data)
+
+
+def delete_encrypted_file(file_id):
+    file_path = f'encrypted_files/{file_id}'
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+
+def delete_decrypted_file(file_id):
+    file_path = f'tmp/{file_id}'
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
